504f6839d53548258fa4b01f4391a22a14c8c921
davi-ga <contato@daviga.dev.br>
2025-07-13 20:42:37 -0300

    feat: restructure user model and add authentication module
    - Deleted the old User table migration and updated the User model in schema.prisma to include email, password, and additional fields.
    - Removed AppService and its corresponding tests, refactoring AppController to use PrismaService instead.
    - Created AuthModule with AuthController and AuthService, including tests for both.
    - Introduced UserModule with UserController and UserService, implementing user creation functionality.
    - Added DatabaseModule to provide PrismaService across the application.
    - Created tests for UserController and UserService to ensure proper functionality.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './database/prisma.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env',
+      envFilePath: '.env'
     }),
+    AuthModule,
+    UserModule,
+    DatabaseModule
   ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
